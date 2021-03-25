7b1e6ed54845430ae2dcb04c82c5510dc5199ab0
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-25 19:04:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,8 @@ class CashRegister
 
   attr_accessor :discount, :total
 
-  def initialize(discount=0)
+  def initialize(discount=0) #optionally takes a discount means that if there is NO discount then the discount is ZERO
+    #which is why we set the discount parameter to 0
     @total = 0
     @discount = discount
   end
